43af6fa563d83fc62195fdbf9d45bc787be4fd79
Evgeniy Khramkov <ewgeniux@gmail.com>
2017-01-12 17:53:07 +0300

    basic routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,28 @@
+import 'hammerjs'
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import 'hammerjs'
+import { RouterModule, Routes } from '@angular/router';
 import { MaterialModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+
+const routes: Routes = [
+  { path: '', component: AuthComponent },
+  { path: 'products', component: AppComponent },
+  { path: 'products/:id', component: AppComponent },
+  { path: 'products/:id/edit', component: AppComponent },
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
     FormsModule,
     HttpModule,
